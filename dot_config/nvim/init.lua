ff72998dd77ceaa6f0eaa7f1b6b5cd734391c7a1
Tunahan Karlibas <dvt.tnhn.krlbs@icloud.com>
2025-01-02 00:34:25 +0300

    nvim: setup rust

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -764,6 +764,7 @@ require('lazy').setup({
       local luasnip = require 'luasnip'
       luasnip.config.setup {}
 
+      --- @diagnostic disable-next-line: missing-fields
       cmp.setup {
         snippet = {
           expand = function(args)
@@ -929,6 +930,7 @@ require('lazy').setup({
         'query',
         'vim',
         'vimdoc',
+        'rust',
       },
       -- Autoinstall languages that are not installed
       auto_install = true,
