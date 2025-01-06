4b3d6c0983ac5c424a724ff9571cda587cd80107
Tunahan Karlibas <tunahan.karlibas@ensmart.com.tr>
2025-01-06 08:30:19 +0300

    nvim: add sql-formatter

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -671,6 +671,7 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'sql-formatter',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
@@ -723,6 +724,7 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        sql = { 'sql-formatter' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
